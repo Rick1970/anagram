5814dc1054516eb31142c72aed549e05cf2e47d5
Rick Thornbrugh and Shokouh Farvid <rick+shokouh@>
2016-07-19 16:45:47 -0700

    add test, and working method

--- Objects/Word.cs
+++ Objects/Word.cs
@@ -1,12 +1,30 @@
 // using System.Collections.Generic;
-
+using System;
 namespace Anagram
 {
   public class Word
   {
-    public string Sort(string alphabet)
+    public  string Evaluate(string word1, string word2)
     {
-      return alphabet;
+      char[] char1 = word1.ToLower().ToCharArray();
+      Array.Sort(char1);
+      string NewWord1 = new string(char1);
+
+      char[] char2 = word2.ToLower().ToCharArray();
+      Array.Sort(char2);
+      string NewWord2 = new string(char2);
+      if (NewWord1 == NewWord2)
+      {
+        return word2;
+      }
+      else
+      {
+        return null;
+      }
     }
+
+
+
   }
 }
+// "good, bad, oogd, low"
--- Test/WordTest.cs
+++ Test/WordTest.cs
@@ -1,17 +1,45 @@
 using Xunit;
-// using System.Collections.Generic;
-
+using System.Collections.Generic;
+using System;
 namespace Anagram
 {
   public class WordTest
   {
     [Fact]
-    public void Sort_CompareAandA_true()
+    public void Evaluate_CompareAandA_sameWord_true()
     {
-      Word testalphabet = new Word() {};
-      string actualalphabet = "a";
-      Assert.Equal(actualalphabet, testalphabet.Sort("b"));
-    }
+      Word testWord = new Word() {};
+      string word1 ="good";
+      string word2 = "good";
+      Assert.Equal("good", testWord.Evaluate(word1, word2));
+     }
 
+     [Fact]
+     public void Evaluate_CompareAandA_differentWord_true()
+     {
+       Word testWord = new Word() {};
+       string word1 ="good";
+       string word2 = "oogd";
+       Assert.Equal("oogd", testWord.Evaluate(word1, word2));
+      }
+
+      [Fact]
+      public void Evaluate_CompareAandA_differentWord_false()
+      {
+        Word testWord = new Word() {};
+        string word1 ="good";
+        string word2 = "bad";
+        Assert.Equal(null, testWord.Evaluate(word1, word2));
+       }
+  //   [Fact]
+  //   public void Sort_CompareInput1Input2_true()
+  //  {
+  //      Word testWord = new Word() {};
+   //
+  //      Assert.Equal();
+   //
+   //
+   //
+  //  }
   }
 }
